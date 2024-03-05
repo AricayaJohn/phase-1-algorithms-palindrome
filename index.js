240d61cf07ae34ab87f9262933586ade5e6df515
John <jaricaya@gmail.com>
2024-03-05 12:14:50 -0800

    Palindrome Lab complete

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const words = word.split("");
+  const reverseWordsSplitArr = words.reverse();
+  const reversedWord = reverseWordsSplitArr.join("");
+  if (word === reversedWord) {
+    return true;
+  }
+  return false;
+} 
 
 /* 
   Add your pseudocode here
-*/
+*/ 
+// step 1 : split the words into an array 
+// step 2 : reverse the words 
+// step 3: make sure to combine array
+// step 4: create if statement to check if words = new words 
 
 /*
   Add written explanation of your solution here
